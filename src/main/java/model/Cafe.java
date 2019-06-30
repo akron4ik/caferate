252374bb0d5cb25d0fc0c56a265da53dc265b29b
Akron4ik <aronov.ak@gmail.com>
2019-06-30 21:58:08 +0300

    second commit

--- src/main/java/model/Cafe.java
+++ src/main/java/model/Cafe.java
@@ -1,4 +1,63 @@
 package model;
 
+import java.util.List;
+
 public class Cafe {
+
+    private int id;
+    private String name;
+    private List<String> menu;
+    private int rating;
+
+    public Cafe() {
+    }
+
+    public Cafe(int id, String name, List<String> menu, int rating) {
+        this.id = id;
+        this.name = name;
+        this.menu = menu;
+        this.rating = rating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getMenu() {
+        return menu;
+    }
+
+    public void setMenu(List<String> menu) {
+        this.menu = menu;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return "Cafe{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", menu=" + menu +
+                ", rating=" + rating +
+                '}';
+    }
 }
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,49 @@
 package model;
 
 public class User {
+    private int id;
+    private String name;
+    private boolean appreciated;
+
+    public User() {
+    }
+
+    public User(int id, String name, boolean appreciated) {
+        this.id = id;
+        this.name = name;
+        this.appreciated = appreciated;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isAppreciated() {
+        return appreciated;
+    }
+
+    public void setAppreciated(boolean appreciated) {
+        this.appreciated = appreciated;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", appreciated=" + appreciated +
+                '}';
+    }
 }
